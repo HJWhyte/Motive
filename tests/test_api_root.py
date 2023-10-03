ed31e4b2bcc0e95f7bac40f62c0e87d6059a5351
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-10-03 12:13:46 +0100

    Adjusted main path im testing

--- tests/test_api_root.py
+++ tests/test_api_root.py
@@ -3,12 +3,13 @@ import requests
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-sys.path.insert(0, r"/..")
+sys.path.insert(0, r"..")
 from main import app  # noqa: E402, E501,E501 pylint: disable=C0413
 
 client = TestClient(app)
 
 def test_can_call_endpoint():
+    '''Function to test if the api endpoint '''
     response = client.get("/")
     assert response.status_code == 200
     pass
