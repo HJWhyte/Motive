1dbb4d427ec4f4b490bf7152d4123e65e723fd2a
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-10-04 16:44:16 +0100

    Created working fail test function, need to clear test cases from database

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -11,16 +11,26 @@ client = TestClient(app)
 @pytest.mark.parametrize("username", ["user1", "user2", "user3"])
 def test_create_user_success(username):
     '''Function to test the createUser route works as intended'''
-    response = client.post(f"/createUser?username={username}")
     
+    response = client.post(f"/createUser?username={username}")
     # Check if the response status code is 200 (OK)
     assert response.status_code == 200
-    
     # Check the response content
     response_json = response.json()
     assert response_json["message"] == "User created successfully"
     assert response_json["username"] == username
-    
     # Ensure user_id is present in the response
     assert "user_id" in response_json  
+
+def test_create_user_success():
+    '''Function to test the createUser route fails when intended'''
     
+    username ='FAILTEST'
+    # Make 2 post requests with the same username
+    response1 = client.post(f"/createUser?username={username}")
+    response2 = client.post(f"/createUser?username={username}")
+    # Check the status code is 400 (Fail)
+    assert response2.status_code == 400
+    #Check Response Content
+    response_json = response2.json()
+    assert "User creation failed" in response_json["detail"]
