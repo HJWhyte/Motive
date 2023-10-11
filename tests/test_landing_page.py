6b5c52de61c4a21d9964189aa6cefe6bcc8830f3
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-10-11 11:12:48 +0100

    test file created and working locally

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -7,3 +7,10 @@ sys.path.append('..')
 from app.main import app
 
 client = TestClient(app)
+
+def test_get_motive():
+    """Test the home page basic functionality"""
+    response = client.get('/motive')
+    response_json = response.json()
+    assert response_json["Message"] == "Welcome to Motive!"
+    assert response.status_code == 200
