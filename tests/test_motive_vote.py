1a0e865ea3fd864d481f2c721ad5efd0d34d1f43
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-10-18 17:57:24 +0100

    Added test to ensure invalid dates fail

--- tests/test_motive_vote.py
+++ tests/test_motive_vote.py
@@ -30,22 +30,23 @@ def test_motive_vote_success():
     clear_test_users(db_users)
     db_close(db_client)
 
-# def test_motive_username_votes_twice_fail():
-#     """Test that an event will not allow the same user to vote on it twice"""
+def test_motive_invalid_date_fail():
+    """Test that an event will not allow the same user to vote on it twice"""
 
-#     name = "TESTEVENT"
-#     username = "TESTUSER1"
-#     start = "2000-01-08"
-#     end = "2000-01-20"
-#     availability = ["2000-01-10", "2000-01-15"]
+    name = "TESTEVENT"
+    username = "TESTUSER1"
+    start = "2000-01-08"
+    end = "2000-01-20"
+    availability = ["2000-02-10", "2000-02-15"]
 
-#     db_client, db_users, db_events = db_connect()
-#     client.post(f"/createUser?username={username}")
-#     client.post(f"/createMotive?motive_name={name}&start_date={start}&end_date={end}")
-#     response1 = client.post(f"/vote/{name}?motive_name={name}&username={username}&availability={availability[0]}&availability={availability[1]}")
-#     response2 = client.post(f"/vote/{name}?motive_name={name}&username={username}&availability={availability[0]}&availability={availability[1]}")
-#     assert response2.status_code == 400
-#     clear_test_events(db_events)
-#     clear_test_users(db_users)
-#     db_close(db_client)
+    db_client, db_users, db_events = db_connect()
+    client.post(f"/createUser?username={username}")
+    client.post(f"/createMotive?motive_name={name}&start_date={start}&end_date={end}")
+    response1 = client.post(f"/vote/{name}?motive_name={name}&username={username}&availability={availability[0]}&availability={availability[1]}")
+    response_json = response1.json()
+    assert response1.status_code == 400
+    assert 'Availability dates must be within the event\'s date range' in response_json["detail"]
+    clear_test_events(db_events)
+    clear_test_users(db_users)
+    db_close(db_client)
 
